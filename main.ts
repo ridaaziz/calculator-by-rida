5a45ad254423b02c78666e5ff52abaeb49af7d65
ridaaziz <rida36267@gmail.com>
2024-04-27 07:47:45 -0700

    calculator by rida

---
+++ main.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+{message:"Enter first number",type:"number",name:"firstnumber"},
+{message:"Enter second number",type:"number",name:"secondnumber"},
+{message:"please select one operator",
+    type:"list",
+    name:"operator",
+    choices:["Addition","multiplication","subtraction","division"]
+
+}
+]);
+// conditional statement
+if(answer.operator === "Addition"){
+    console.log(answer.firstnumber + answer.secondnumber)
+}
+else if(answer.operator === "multiplication"){
+    console.log(answer.firstnumber * answer.secondnumber)
+}
+else if(answer.operator === "subtraction"){
+    console.log(answer.firstnumber - answer.secondnumber)
+}
+else if(answer.operator === "division"){
+    console.log(answer.firstnumber / answer.secondnumber)
+}else(console.log("please select operator"))
